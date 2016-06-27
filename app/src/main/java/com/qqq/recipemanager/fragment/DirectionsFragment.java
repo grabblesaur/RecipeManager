1d69b7b77661585c3e1fd0954450855b8f184004
grabblesaur <bogdanov.bayar@gmail.com>
2016-06-27 21:17:18 +0900

    added ViewPager

---
+++ app/src/main/java/com/qqq/recipemanager/fragment/DirectionsFragment.java
@@ -0,0 +1,22 @@
+package com.qqq.recipemanager.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.qqq.recipemanager.R;
+
+/**
+ * Created by qqq on 27.06.2016.
+ */
+public class DirectionsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_directions, container, false);
+        return view;
+    }
+}
